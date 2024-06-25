353b554d531359fcc56c09f4945c1848cc167160
dkl096 <144052734+dkl096@users.noreply.github.com>
2024-06-25 15:31:59 +0800

    removed js files with case problem

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Home from "./main/Home";
 import Contact from "./main/Contact.js";
 
 function Main() {
   return (
     <div>
-      <Home />
-      <About />
-      <Experience />
       <Contact />
     </div>
   );
